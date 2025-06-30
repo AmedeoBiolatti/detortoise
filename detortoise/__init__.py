e4bf5a916d59feda8334448950a39ce94d9e95f9
Amedeo Biolatti <amedeo.biolatti@gmail.com>
2025-07-01 00:26:36 +0200

    initial commit

---
+++ detortoise/__init__.py
@@ -0,0 +1,1 @@
+from detortoise import layers
---
+++ detortoise/layers/__init__.py
@@ -0,0 +1,4 @@
+from detortoise.layers.dora import DoRAWrapper
+from detortoise.layers.loha import LoHAWrapper
+from detortoise.layers.lora import LoRAWrapper
+from detortoise.layers.quantized import NF4
